2cc195279516b09b465e2fb523a945ab9eb17e11
Dmitry Lyapin <lyapindm@yandex.ru>
2023-02-21 13:58:52 +0300

    Установил библиотеки компонент

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,31 +7,42 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
     return (
         <main className={styles.main}>
-            <div className={styles.description}>
-                <p>
-                    Get started by editing&nbsp;
-                    <code className={styles.code}>src/app/page.tsx</code>
-                </p>
-                <div>
-                </div>
-            </div>
+            <section className=" text-white">
+                <div
+                    className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+                >
+                    <div className="mx-auto max-w-3xl text-center">
+                        <h1
+                            className="bg-gradient-to-r from-purple-600  via-blue-500 to-green-300 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+                        >
+                            Understand User Flow.
 
-            <div className={styles.center}>
-                <Image
-                    className={styles.logo}
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
-                    priority
-                />
-                <div className={styles.thirteen}>
-                    <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-                </div>
-            </div>
+                            <span className="sm:block"> Increase Conversion. </span>
+                        </h1>
 
-            <div className={styles.grid}>
-            </div>
+                        <p className="mx-auto mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
+                            tenetur fuga ducimus numquam ea!
+                        </p>
+
+                        <div className="mt-8 flex flex-wrap justify-center gap-4">
+                            <a
+                                className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                                href="/get-started"
+                            >
+                                Get Started
+                            </a>
+
+                            <a
+                                className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+                                href="/about"
+                            >
+                                Learn More
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
