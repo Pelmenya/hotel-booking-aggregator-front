6c8b444615e3a062a5ba94890e054fd7eab787f2
Dmitry Lyapin <lyapindm@yandex.ru>
2023-09-09 12:43:23 +0300

    Сделал footer 1.0

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,10 @@
 import { getYear } from 'utils/getYear';
 import { Logo } from '../logo/logo';
 import Link from 'next/link';
+import VKIcon from '@/icons/social/vk.svg';
+import TGIcon from '@/icons/social/tg.svg';
+import GitHubIcon from '@/icons/social/git-hub.svg';
+import WhatsAppIcon from '@/icons/social/whats-app.svg';
 
 export const Footer = () => {
     const currentYear = getYear();
@@ -11,50 +15,56 @@ export const Footer = () => {
                 <aside>
                     <Logo />
                     <p>
-                        © 2023 {currentYear > 2023 && <span>- {currentYear}</span>} Дмитрий Ляпин
+                        © 2023{' '}
+                        {currentYear > 2023 && <span>- {currentYear}</span>}{' '}
+                        Дмитрий Ляпин
                     </p>
                 </aside>
                 <nav>
                     <header className="footer-title">Информация</header>
-                    <div className="grid grid-flow-col gap-4">
-                        <Link href="/privacy-policy" className="link link-hover">Политика конфиденциальности</Link>
-                    </div>
+                    <Link href="/privacy-policy" className="link link-hover">
+                        Политика конфиденциальности
+                    </Link>
+                </nav>
+                <nav>
+                    <header className="footer-title">Контакты</header>
+                    <a href="tel:+79166198852" className="link link-hover">
+                        Тел.: +7(916)-619-88-52
+                    </a>
+                    <a href="mailto:lyapindm@yandex.ru" className="link link-hover">
+                        Email.: lyapindm@yandex.ru
+                    </a>
                 </nav>
                 <nav>
                     <header className="footer-title">Соцсети</header>
                     <div className="grid grid-flow-col gap-4">
-                        <a>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                className="fill-current"
-                            >
-                                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                            </svg>
+                        <a
+                            href="https://vk.com/lyapindm"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <VKIcon />
                         </a>
-                        <a>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                className="fill-current"
-                            >
-                                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                            </svg>
+                        <a
+                            href="https://t.me/dmitry_lyapin"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <TGIcon />
                         </a>
-                        <a>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                className="fill-current"
-                            >
-                                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                            </svg>
+                        <a
+                            href="https://github.com/Pelmenya"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <GitHubIcon />
+                        </a>
+                        <a
+                            href="https://wa.me/+79166198852"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <WhatsAppIcon />
                         </a>
                     </div>
                 </nav>
--- src/components/header/notifications-link/notifications-link.tsx
+++ src/components/header/notifications-link/notifications-link.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 
 export const NotificationsLink = () => (
-    <Link
-        href='/notifications'
+    <button
+        disabled
         className="rounded-full bg-neutral p-1 text-neutral-content hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
     >
         <span className="sr-only">View notifications</span>
@@ -20,5 +20,5 @@ export const NotificationsLink = () => (
                 d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
             />
         </svg>
-    </Link>
+    </button>
 );
