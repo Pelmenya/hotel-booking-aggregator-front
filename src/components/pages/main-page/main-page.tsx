8263124cd9e834e530846eac5b5c40ccbccb7353
Dmitry Lyapin <lyapindm@yandex.ru>
2023-09-14 21:38:25 +0300

    Fix err

--- src/components/pages/main-page/main-page.tsx
+++ src/components/pages/main-page/main-page.tsx
@@ -4,8 +4,8 @@ import { CarouselVideo, TSlide } from '@/components/carousel-video/carousel-vide
 
 const sls: TSlide[] = [
     { url: 'tropical.mp4', head: 'Окунитесь в мир приключений' },
-    { url: 'forest.mp4', head: 'Мир изысконной природы' },
-    { url: 'agua.mp4', head: 'TOP-HOTELS.SU - твой путь'},
+    { url: 'forest.mp4', head: 'Мир изысканной природы' },
+    { url: 'agua.mp4', head: 'TOP-HOTELS.SU - Ваш путь'},
 ];
 
 export const MainPage = () => {
