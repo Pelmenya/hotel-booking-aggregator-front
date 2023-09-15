c07ccea2749587ba5057d0e2daebeb4079ac5d9a
Dmitry Lyapin <lyapindm@yandex.ru>
2023-09-15 16:10:19 +0300

    Добавил Яндекс.Метрика

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -2,7 +2,15 @@ import { TBaseProps } from '@/types/t-base-props';
 import Head from 'next/head';
 import { Header } from '../components/header/header';
 import { Footer } from '@/components/footer/footer';
-import { useRouter } from 'next/router';
+import Router, { useRouter } from 'next/router';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
+
+Router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+        ym('hit', url);
+    }
+});
 
 export type ILayoutProps = TBaseProps & {
     title: string;
@@ -10,6 +18,12 @@ export type ILayoutProps = TBaseProps & {
 
 export const Layout = ({ title, children }) => {
     const router = useRouter();
+    const {
+        NEXT_PUBLIC_SITE_URL, 
+        NEXT_PUBLIC_BASE_IMAGES_URL, 
+        NEXT_PUBLIC_YM_ACCOUNT
+    } = process.env;
+
     return (
         <>
             <Head>
@@ -17,16 +31,29 @@ export const Layout = ({ title, children }) => {
                 <link rel="icon" href="logo.svg" />
                 <meta
                     property="og:url"
-                    content={process.env.NEXT_PUBLIC_SITE_URL + router.asPath}
+                    content={NEXT_PUBLIC_SITE_URL + router.asPath}
                 />
                 <meta property="og:locale" content="ru_RU" />
-                <meta property="og:image"  content={process.env.NEXT_PUBLIC_BASE_IMAGES_URL + 'og-img.png'}/>
-                <meta property="og:og:title" content={title.split('~')[1].trim()}/>
+                <meta
+                    property="og:image"
+                    content={
+                        NEXT_PUBLIC_BASE_IMAGES_URL + 'og-img.png'
+                    }
+                />
+                <meta
+                    property="og:og:title"
+                    content={title.split('~')[1].trim()}
+                />
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1"
                 />
             </Head>
+            <YMInitializer
+                accounts={[Number(NEXT_PUBLIC_YM_ACCOUNT)]}
+                options={{ webvisor: true, defer: true }}
+                version="2"
+            />
             <Header />
             <main className="container mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="w-full min-h-main">{children}</div>
