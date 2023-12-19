9be15f58402f4d768d3c2bd402373ee3a9800aa0
Dmitry Lyapin <lyapindm@yandex.ru>
2023-12-19 09:08:54 +0300

    Переделал переключатель цветовых тем

--- src/components/header/components/theme-toggle/theme-toggle.tsx
+++ src/components/header/components/theme-toggle/theme-toggle.tsx
@@ -1,20 +1,22 @@
 import { useCallback, useEffect, useState } from 'react';
 import { themeChange } from 'theme-change';
+import SunIcon from '@/icons/sun.svg';
+import MoonIcon from '@/icons/moon.svg';
 
 export const ThemeToggle = () => {
-    const [isChecked, setIsChecked] = useState(false);
+    const [isChecked, setIsChecked] = useState(true);
 
     useEffect(() => {
         themeChange(false);
         const localStorageTheme = localStorage.getItem('theme');
         if (localStorageTheme) {
             if (localStorageTheme === 'dark') {
-                setIsChecked(true);
-            }
-            if (localStorageTheme === 'light') {
                 setIsChecked(false);
             }
-        } else setIsChecked(false);
+            if (localStorageTheme === 'light') {
+                setIsChecked(true);
+            }
+        } else setIsChecked(true);
     }, []);
 
     const handlerSetIsChecked = useCallback(
@@ -23,33 +25,21 @@ export const ThemeToggle = () => {
     );
 
     return (
-        <div className="form-control items-end">
-            <label className="cursor-pointer label">
-                <span className="label-text rounded-full bg-neutral p-1 text-neutral-content hover:text-white">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-                        />
-                    </svg>
-                </span>
+        <div className="form-control items-end text-neutral-content hover:text-white">
+            <label className="swap swap-rotate">
                 <input
-                    id="ToggleTheme"
                     type="checkbox"
-                    className="toggle toggle-primary toggle-sm hidden"
+                    className="theme-controller"
+                    value="light"
+                    id="ToggleTheme"
                     checked={isChecked}
                     data-toggle-theme="dark,light"
                     data-act-class="ACTIVECLASS"
                     onChange={handlerSetIsChecked}
+
                 />
+                <SunIcon />
+                <MoonIcon />
             </label>
         </div>
     );
