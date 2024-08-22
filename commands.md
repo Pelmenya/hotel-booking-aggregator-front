d3a84d42bbefb8ae17f7ad13ff6bc71ca0fe03c5
Dmitry Lyapin <lyapindm@yandex.ru>
2024-08-22 13:49:26 +0300

    Разработка ограничения на общий размер файлов при одной пересылке

--- commands.md
+++
@@ -1,3 +0,0 @@
-* docker compose -f docker-compose.yml up --build
-* docker compose up --build
-* docker exec -it hotel-aggregator-front bash
--- src/components/forms/components/input-file/input-file.tsx
+++ src/components/forms/components/input-file/input-file.tsx
@@ -3,6 +3,9 @@ import { Controller } from 'react-hook-form';
 import { IInputProps } from '../input/input';
 import cn from 'classnames';
 
+const maxFilesValue = Number(process.env.NEXT_PUBLIC_MAX_FILES) || 4;
+const maxFilesSizeValue = Number(process.env.NEXT_PUBLIC_MAX_FILES_SIZE) || 1048576;
+
 export const InputFile = ({
     id,
     placeholder,
@@ -13,19 +16,55 @@ export const InputFile = ({
     control,
     reset,
     className,
+    maxFiles = maxFilesValue , // максимальное количество файлов
+    maxFileSize = maxFilesSizeValue, // максимальный размер всех файлов в байтах (по умолчанию 1 МБ) за один раз
 }: Partial<
     IInputProps & {
         reset?: boolean;
         handlerOnChange: (e: ChangeEvent<HTMLInputElement>) => void;
+        maxFiles?: number;
+        maxFileSize?: number;
     }
 >) => {
-    const [display, setDisplay] = useState('Файл не выбран');
+    const fileNotSelected = 'Файл не выбран'
+    const [display, setDisplay] = useState(fileNotSelected);
+    const [error, setError] = useState('')
 
     useEffect(() => {
         if (reset) {
-            setDisplay('Файл не выбран');
+            setDisplay(fileNotSelected);
         }
     }, [reset]);
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+        if (files) {
+            if (files.length > maxFiles) {
+                alert(`Можно загрузить не более ${maxFiles} файла(ов).`);
+                e.target.value = ''; // сбросить выбор
+                setDisplay(fileNotSelected);
+                return;
+            }
+            for (let i = 0; i < files.length; i++) {
+                if (files[i].size > maxFileSize) {
+                    alert(`Размер файла ${files[i].name} превышает допустимый лимит в ${maxFileSize / 1048576} МБ.`);
+                    e.target.value = ''; // сбросить выбор
+                    setDisplay(fileNotSelected);
+                    return;
+                }
+            }
+            if (files.length === 1) {
+                setDisplay(files[0].name);
+            } else if (files.length > 1) {
+                setDisplay(`Число файлов: ${files.length}`);
+            } else {
+                setDisplay(fileNotSelected);
+            }
+        } else {
+            setDisplay(fileNotSelected);
+        }
+        handlerOnChange && handlerOnChange(e);
+    };
 
     return (
         <Controller
@@ -45,21 +84,7 @@ export const InputFile = ({
                         value={value}
                         title=""
                         onBlur={onBlur}
-                        onChange={(e) => {
-                            const files = e.target.files;
-                            if (files) {
-                                if (files.length === 1) {
-                                    setDisplay(files[0].name);
-                                } else if (files.length > 1) {
-                                    setDisplay(`Число файлов: ${files.length}`);
-                                } else {
-                                    setDisplay('Файл не выбран');
-                                }
-                            } else {
-                                setDisplay('Файл не выбран');
-                            }
-                            handlerOnChange && handlerOnChange(e);
-                        }}
+                        onChange={handleFileChange}
                         multiple={multiple}
                         accept={accept}
                     />
