356916a8af93815cfcc07d768ba98367f434827d
Dmitry Lyapin <lyapindm@yandex.ru>
2024-10-03 14:03:57 +0300

    Адаптив Edit User Form

--- src/components/tab/components/tab-content/tab-content.tsx
+++ src/components/tab/components/tab-content/tab-content.tsx
@@ -1,5 +1,5 @@
 import { TBaseProps } from '@/types/t-base-props';
 
 export const TabContent = ({ children }: TBaseProps) => (
-    <div className="grid grid-cols-1 grid-rows-4 sm:grid-rows-1 sm:grid-cols-3 lg:grid-cols-4 gap-4 h-full">{children}</div>
+    <div className="grid grid-cols-1 grid-rows-4 md:grid-rows-1 md:grid-cols-3 lg:grid-cols-4 gap-4 h-full">{children}</div>
 );
--- src/components/tab/components/tab-menu/components/tab-menu-wrapper/tab-menu-wrapper.tsx
+++ src/components/tab/components/tab-menu/components/tab-menu-wrapper/tab-menu-wrapper.tsx
@@ -1,7 +1,7 @@
 import { TBaseProps } from '@/types/t-base-props';
 
 export const TabMenuWrapper = ({ children }: TBaseProps) => (
-    <div className="col-span-1 row-span-1 border border-base-300 rounded-[0.375rem] shadow-md flex flex-col justify-between">
+    <div className="col-span-1 row-span-1 border border-base-300 rounded-[0.375rem] shadow-md flex flex-col justify-around">
         {children}
     </div>
 );
