a4d9ba817bd371e145f98368d9534b0d39e0a525
Dmitry Lyapin <lyapindm@yandex.ru>
2024-10-03 14:15:19 +0300

    Адаптив Edit User Form

--- src/components/tab/components/tab-content/tab-content-main/tab-content-main.tsx
+++ src/components/tab/components/tab-content/tab-content-main/tab-content-main.tsx
@@ -2,7 +2,7 @@ import { TBaseProps } from '@/types/t-base-props';
 
 export const TabContentMain = ({ children }: TBaseProps) => {
     return (
-        <div className="row-span-3 md:row-span-1 md:col-span-2 lg:col-span-3 bg-base-100 py-4 rounded-md h-full w-full text-base-content">
+        <div className="md:row-span-1 md:col-span-2 lg:col-span-3 bg-base-100 py-4 rounded-md h-full w-full text-base-content">
             <div className="bg-base-100 py-4 h-full w-full rounded-md flex flex-col items-center justify-start text-base-content">
                 {children}
             </div>
--- src/components/tab/components/tab-content/tab-content.tsx
+++ src/components/tab/components/tab-content/tab-content.tsx
@@ -1,5 +1,5 @@
 import { TBaseProps } from '@/types/t-base-props';
 
 export const TabContent = ({ children }: TBaseProps) => (
-    <div className="grid grid-cols-1 grid-rows-4 md:grid-rows-1 md:grid-cols-3 lg:grid-cols-4 gap-4 h-full">{children}</div>
+    <div className="flex flex-col md:grid md:grid-rows-1 md:grid-cols-3 lg:grid-cols-4 gap-4 h-full">{children}</div>
 );
