c8bc79e9eba1171f7828ab0dd2a6082a10955b55
Dmitry Lyapin <lyapindm@yandex.ru>
2025-01-14 14:56:34 +0300

    Refactoring HotelHead

---
+++ src/components/pages/hotel-page/components/hotel-head.tsx
@@ -0,0 +1,125 @@
+import { useState } from 'react';
+import { Base } from '@/components/base/base'
+import { CarouselFullPicPreview } from '@/components/carousel-full-pic-preview/carousel-full-pic-preview';
+import { ImagesGrid } from '@/components/images-grid/images-grid';
+import { Modal } from '@/components/modal/modal';
+import { useTranslation } from 'react-i18next';
+import { THotelPageProps } from '../hotel-page'
+import Rating from '../../../../icons/hand-thumb-up.svg';
+
+export type THotelHeadProps = THotelPageProps & {
+    hotelAddress: string;
+    hotelName: string;
+}
+
+export const HotelHead = ({ data, hotelAddress, hotelName }: THotelHeadProps) => {
+    const { i18n } = useTranslation();
+    const [isOpenModal, setIsOpenModal] = useState(false);
+
+    const handlerToogleModal = () => {
+        setIsOpenModal(!isOpenModal);
+    };
+
+    return (
+        <Base>
+            {data?.images.length ? (
+                <Modal
+                    isOpen={isOpenModal}
+                    handlerClose={handlerToogleModal}
+                >
+                    <CarouselFullPicPreview
+                        images={data?.images || []}
+                        name={hotelName}
+                    />
+                </Modal>
+            ) : null}
+            <div className="flex flex-col gap-1">
+                <div className="flex flex-col gap-2">
+                    <h1 className="font-black text-2xl md:text-2xl lg:text-4xl">
+                        {hotelName}
+                    </h1>
+                    <p className="text-primary">{hotelAddress}</p>
+                    {data?.amenities && Array.isArray(data.amenities.ru) && Array.isArray(data.amenities.en) && data.amenities.en.length > 0 ? (
+                        <div className="flex gap-2 flex-wrap">
+                            {i18n.language === 'ru'
+                                ? data?.amenities.ru
+                                    .filter(
+                                        (item) => item.type === 'main'
+                                    )[0]
+                                    .amenities_list.map((item) => (
+                                        <p
+                                            className="badge badge-sm md:badge-md badge-ghost"
+                                            key={item.idx}
+                                        >
+                                            {item.name}
+                                        </p>
+                                    ))
+                                : data?.amenities.en
+                                    .filter(
+                                        (item) => item.type === 'main'
+                                    )[0]
+                                    .amenities_list.map((item) => (
+                                        <p
+                                            className="badge badge-sm md:badge-md badge-ghost"
+                                            key={item.idx}
+                                        >
+                                            {item.name}
+                                        </p>
+                                    ))}
+                        </div>
+                    ) : null}
+                    {data?.geoData && Array.isArray(data.geoData.ru) && Array.isArray(data.geoData.en) && data.geoData.en.length > 0 ? (
+                        <div>
+                            {i18n.language === 'ru'
+                                ? data?.geoData.ru
+                                    .filter(
+                                        (item) => item.type === 'head'
+                                    )[0]
+                                    .geo_list.map((item) => (
+                                        <p
+                                            className="text-warning text-xs"
+                                            key={item.idx}
+                                        >
+                                            {item.name.trim()} ~{' '}
+                                            {item.distance_from_hotel}{' '}
+                                            {item.measurement}
+                                        </p>
+                                    ))
+                                : data?.geoData.en
+                                    .filter(
+                                        (item) => item.type === 'head'
+                                    )[0]
+                                    .geo_list.map((item) => (
+                                        <p
+                                            className="text-warning text-xs"
+                                            key={item.idx}
+                                        >
+                                            {item.name} ~{' '}
+                                            {item.distance_from_hotel}{' '}
+                                            {item.measurement}
+                                        </p>
+                                    ))}
+                        </div>
+                    ) : (
+                        <></>
+                    )}
+                    <>
+                        {data?.hotel.rating && data?.hotel.rating > 0 ? (
+                            <p className="flex items-center gap-1">
+                                <Rating /> {data?.hotel.rating}
+                            </p>
+                        ) : null}
+                    </>
+                </div>
+                <div className="divider"></div>
+                {data?.images.length ? (
+                    <ImagesGrid
+                        onClick={handlerToogleModal}
+                        images={data.images}
+                        name={hotelName}
+                    />
+                ) : null}
+            </div>
+        </Base>
+    )
+}
--- src/components/pages/hotel-page/hotel-page.tsx
+++ src/components/pages/hotel-page/hotel-page.tsx
@@ -1,26 +1,26 @@
-import { useMemo, useState } from 'react';
-import { CarouselFullPicPreview } from '@/components/carousel-full-pic-preview/carousel-full-pic-preview';
-import { ImagesGrid } from '@/components/images-grid/images-grid';
-import { Modal } from '@/components/modal/modal';
+import { useMemo } from 'react';
 import { THotelResData } from '@/types/t-hotel-res-data';
 import { Map } from '@/components/map/map';
 import { TNullable } from '@/types/t-nullable';
 import { useTranslation } from 'react-i18next';
 import { Collapse } from '@/components/collapse/collapse';
-import { Base } from '@/components/base/base';
-import Rating from '../../../icons/hand-thumb-up.svg';
+import {HotelHead } from './components/hotel-head';
 
 export type THotelPageProps = {
     data: TNullable<THotelResData>;
 };
 export const HotelPage = ({ data }: THotelPageProps) => {
     const { i18n } = useTranslation();
-    const [isOpenModal, setIsOpenModal] = useState(false);
-    //const descriptions: string[] = hotel.description.split('\r\n');
 
-    const handlerToogleModal = () => {
-        setIsOpenModal(!isOpenModal);
-    };
+    const aboutsTitle = useMemo(
+        () =>
+            data && data.abouts
+                ? i18n.language === 'ru'
+                    ? data?.abouts.ru?.title || 'Описание'
+                    : data?.abouts.en?.title || 'Description'
+                : '',
+        [data, i18n.language]
+    );
 
     const hotelName = useMemo(
         () =>
@@ -48,138 +48,61 @@ export const HotelPage = ({ data }: THotelPageProps) => {
         [data, i18n.language]
     );
 
-    const aboutsTitle = useMemo(
-        () =>
-            data && data.abouts
-                ? i18n.language === 'ru'
-                    ? data?.abouts.ru?.title || 'Описание'
-                    : data?.abouts.en?.title || 'Description'
-                : '',
-        [data, i18n.language]
-    );
     return (
         <>
-            <div className="p-8">Breadcrumbs</div>
+            {/* <div className="p-8">Breadcrumbs</div> */}   
+            <div className="p-4"></div>
             <div className="flex flex-col gap-4">
-                <Base>
-                    {data?.images.length ? (
-                        <Modal
-                            isOpen={isOpenModal}
-                            handlerClose={handlerToogleModal}
-                        >
-                            <CarouselFullPicPreview
-                                images={data?.images || []}
-                                name={hotelName}
-                            />
-                        </Modal>
-                    ) : null}
-                    <div className="flex flex-col gap-1">
-                        <div className="flex flex-col gap-2">
-                            <h1 className="font-black text-xl md:text-2xl lg:text-4xl">
-                                {hotelName}
-                            </h1>
-                            <p className="text-primary">{hotelAddress}</p>
-                            {data?.amenities && Array.isArray(data.amenities.ru) && Array.isArray(data.amenities.en) && data.amenities.en.length > 0 ? (
-                                <div className="flex gap-2 flex-wrap">
-                                    {i18n.language === 'ru'
-                                        ? data?.amenities.ru
-                                            .filter(
-                                                (item) => item.type === 'main'
-                                            )[0]
-                                            .amenities_list.map((item) => (
-                                                <p
-                                                    className="badge badge-sm md:badge-md badge-ghost"
-                                                    key={item.idx}
-                                                >
-                                                    {item.name}
-                                                </p>
-                                            ))
-                                        : data?.amenities.en
-                                            .filter(
-                                                (item) => item.type === 'main'
-                                            )[0]
-                                            .amenities_list.map((item) => (
-                                                <p
-                                                    className="badge badge-sm md:badge-md badge-ghost"
-                                                    key={item.idx}
-                                                >
-                                                    {item.name}
-                                                </p>
-                                            ))}
-                                </div>
-                            ) : null}
-                            {data?.geoData && Array.isArray(data.geoData.ru) && Array.isArray(data.geoData.en) && data.geoData.en.length > 0 ? (
-                                <div>
-                                    {i18n.language === 'ru'
-                                        ? data?.geoData.ru
-                                            .filter(
-                                                (item) => item.type === 'head'
-                                            )[0]
-                                            .geo_list.map((item) => (
-                                                <p
-                                                    className="text-warning text-xs"
-                                                    key={item.idx}
-                                                >
-                                                    {item.name.trim()} ~{' '}
-                                                    {item.distance_from_hotel}{' '}
-                                                    {item.measurement}
-                                                </p>
-                                            ))
-                                        : data?.geoData.en
-                                            .filter(
-                                                (item) => item.type === 'head'
-                                            )[0]
-                                            .geo_list.map((item) => (
-                                                <p
-                                                    className="text-warning text-xs"
-                                                    key={item.idx}
-                                                >
-                                                    {item.name} ~{' '}
-                                                    {item.distance_from_hotel}{' '}
-                                                    {item.measurement}
-                                                </p>
-                                            ))}
-                                </div>
-                            ) : (
-                                <></>
-                            )}
-                            <>
-                                {data?.hotel.rating && data?.hotel.rating > 0 ? (
-                                    <p className="flex items-center gap-1">
-                                        <Rating /> {data?.hotel.rating}
-                                    </p>
-                                ) : null}
-                            </>
-                        </div>
-                        <div className="divider"></div>
-                        {data?.images.length ? (
-                            <ImagesGrid
-                                onClick={handlerToogleModal}
-                                images={data.images}
-                                name={hotelName}
-                            />
-                        ) : null}
-                        {/*                 <article className="prose lg:prose-xl">
-                    <h3 className="font-mono">Про отель</h3>
-                    {descriptions.map((text, idx) => (
-                        <p key={text + idx} className="font-mono">
-                            {text}
-                        </p>
-                    ))}
-                </article> */}
-                    </div>
-                </Base>
+                <HotelHead data={data} hotelAddress={hotelAddress} hotelName={hotelName}/>
                 <Collapse title={hotelAddress} type={'arrow'} fullView={true}>
-                    {data?.locations.en.geocode_data.geo_data.mid ? (
-                        <Map
-                            coordinates={[
-                                data.locations.en.geocode_data.geo_data.mid.lat,
-                                data.locations.en.geocode_data.geo_data.mid.lon,
-                            ]}
-                        />
-                    ) : (
-                        <></>
-                    )}
+                    <>
+                        {data?.locations.en.geocode_data.geo_data.mid ? (
+                            <Map
+                                coordinates={[
+                                    data.locations.en.geocode_data.geo_data.mid.lat,
+                                    data.locations.en.geocode_data.geo_data.mid.lon,
+                                ]}
+                            />
+                            
+                        ) : (
+                            <></>
+                        )}
+                        {data?.geoData && Array.isArray(data.geoData.ru) && Array.isArray(data.geoData.en) && data.geoData.en.length > 0 ? (
+                            <div>
+                                {i18n.language === 'ru'
+                                    ? data?.geoData.ru
+                                        .filter(
+                                            (item) => item.type === 'main'
+                                        )[0]
+                                        .geo_list.map((item) => (
+                                            <p
+                                                className="text-warning text-xs"
+                                                key={item.idx}
+                                            >
+                                                {item.name.trim()} ~{' '}
+                                                {item.distance_from_hotel}{' '}
+                                                {item.measurement}
+                                            </p>
+                                        ))
+                                    : data?.geoData.en
+                                        .filter(
+                                            (item) => item.type === 'main'
+                                        )[0]
+                                        .geo_list.map((item) => (
+                                            <p
+                                                className="text-warning text-xs"
+                                                key={item.idx}
+                                            >
+                                                {item.name} ~{' '}
+                                                {item.distance_from_hotel}{' '}
+                                                {item.measurement}
+                                            </p>
+                                        ))}
+                            </div>
+                        ) : (
+                            <></>
+                        )}
+                    </>
                 </Collapse>
                 <>
                     {data && data.abouts?.ru ? (
