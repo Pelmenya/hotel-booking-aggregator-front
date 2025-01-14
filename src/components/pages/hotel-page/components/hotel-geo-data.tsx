0eaaf19e70dbef96a6921afb4efc0be252f386d7
Dmitry Lyapin <lyapindm@yandex.ru>
2025-01-14 18:32:14 +0300

    Flex-auto HotelGeoData geo lists

--- src/components/pages/hotel-page/components/hotel-geo-data.tsx
+++ src/components/pages/hotel-page/components/hotel-geo-data.tsx
@@ -36,8 +36,8 @@ export const HotelGeoData = ({ data, hotelAddress }: THotelHeadProps) => {
                         {(i18n.language === 'ru'
                             ? data.geoData.ru
                             : data.geoData.en
-                        ).filter((geoData) => geoData.type === 'additional').map((geoData) => (
-                            <div key={geoData.id}>
+                        ).filter((geoData) => geoData.type === 'additional').sort((a,b) => a.geo_list.length - b.geo_list.length).map((geoData) => (
+                            <div className="flex-auto" key={geoData.id}>
                                 {renderGeoData(geoData.geo_list, geoData.title)}
                             </div>
                         ))}
